5c3cf31bc489cf811ccc2e6f2eff4a1ddb909d03
Wei Kian <weikian92@hotmail.com>
2026-07-10 09:57:53 +0800

    add toast msg

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers"; // 👈 import this
 import ConditionalLayout from "./conditional-layout";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Providers>
           <ConditionalLayout>{children}</ConditionalLayout>
+          <Toaster />
         </Providers>
       </body>
     </html>
